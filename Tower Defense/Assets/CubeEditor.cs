1af4d3b7f93349dbf08b85dce037ea9b5b9d60ab
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-08 01:05:29 -0800

    Using Text Mesh For Labels
    1. How to use Text Mesh in Unity
    2. Text Mesh vs UI Text
    3. Using the `[SelectionBase]` attribute
    4. Using 'GetComponentInChildren()'

--- Tower Defense/Assets/CubeEditor.cs
+++ Tower Defense/Assets/CubeEditor.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 	[ExecuteInEditMode]
-public class Snap : MonoBehaviour {
+	[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
 	[SerializeField] [Range(1f, 20f)] float gridSize = 10f; // range allows a dragging function from 1 to 20.
 	// you can also manually punch in the number into the float rather than use range also.
 
+	TextMesh textMesh;
+
+	void Start()
+	{
+
+	}
+
 	void Update () {
+	
 		Vector3 snapPosition;
 		snapPosition.x = Mathf.RoundToInt (transform.position.x / gridSize) * gridSize; // transform.position.x is the x position of cube.
 		snapPosition.z = Mathf.RoundToInt (transform.position.z / gridSize) * gridSize;
+		transform.position = new Vector3(snapPosition.x, 0 , snapPosition.z); 
+		
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = snapPosition.x / gridSize + "," + snapPosition.z / gridSize; // text changes according to position of cube.
 
-		transform.position = new Vector3(snapPosition.x, 0 , snapPosition.z);
+		
 
+		
 	}
 } // end of class
