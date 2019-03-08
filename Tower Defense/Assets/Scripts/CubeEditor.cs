4e48d2f51817692ed078ce53d0015352faf3742a
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-08 05:30:49 -0800

    C-Sharp Lists For Custom Path
    1. The `List<>` syntax
    2. Lists vs arrays
    3. Creating a custom waypoint path.

--- Tower Defense/Assets/Scripts/CubeEditor.cs
+++ Tower Defense/Assets/Scripts/CubeEditor.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-	[ExecuteInEditMode]
-	[SelectionBase]
+	[ExecuteInEditMode] // <-- execute in edit mode yeee
+	[SelectionBase] // makes selecting top quad of cube easier, this prevents me from clicking other quads.
 public class CubeEditor : MonoBehaviour {
 
 	[SerializeField] [Range(1f, 20f)] float gridSize = 10f; // range allows a dragging function from 1 to 20.
@@ -19,12 +19,15 @@ public class CubeEditor : MonoBehaviour {
 	void Update () {
 	
 		Vector3 snapPosition;
+		
 		snapPosition.x = Mathf.RoundToInt (transform.position.x / gridSize) * gridSize; // transform.position.x is the x position of cube.
 		snapPosition.z = Mathf.RoundToInt (transform.position.z / gridSize) * gridSize;
 		transform.position = new Vector3(snapPosition.x, 0 , snapPosition.z); 
 		
 		textMesh = GetComponentInChildren<TextMesh>();
-		textMesh.text = snapPosition.x / gridSize + "," + snapPosition.z / gridSize; // text changes according to position of cube.
+		string textLabel =  snapPosition.x / gridSize + "," + snapPosition.z / gridSize; // text changes according to position of cube
+		textMesh.text = textLabel;
+		gameObject.name = textLabel;
 
 		
 
---
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	[SerializeField] List<Block> path;
+
+
+	// Use this for initialization
+	void Start () {
+		PrintAllWayPoints();
+	}
+	
+	private void PrintAllWayPoints()
+	{
+		foreach (Block waypoint in path) // Block becomes waypoint
+		{
+			print(waypoint.name); // name of each cube appear in console.
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
