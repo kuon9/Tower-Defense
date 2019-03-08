4c41c780cbd7b3860f8486906e5ffd7c1e4bc941
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-07 19:25:11 -0800

    [ExecuteInEditMode] Attribute
    1. Why we want snapping in our world.
    2. How to use Unity's snap feature.
    3. The limitations and why we write our own.
    4. Introducing `[ExecuteInEditMode]` attribute.
    5. Introducing `[Range()]` attribute.

---
+++ Tower Defense/Assets/Snap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[ExecuteInEditMode]
+public class Snap : MonoBehaviour {
+
+	[SerializeField] [Range(1f, 20f)] float gridSize = 10f; // range allows a dragging function from 1 to 20.
+	// you can also manually punch in the number into the float rather than use range also.
+
+	void Update () {
+		Vector3 snapPosition;
+		snapPosition.x = Mathf.RoundToInt (transform.position.x / gridSize) * gridSize; // transform.position.x is the x position of cube.
+		snapPosition.z = Mathf.RoundToInt (transform.position.z / gridSize) * gridSize;
+
+		transform.position = new Vector3(snapPosition.x, 0 , snapPosition.z);
+
+	}
+} // end of class
