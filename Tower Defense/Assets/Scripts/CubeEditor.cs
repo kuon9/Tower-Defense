82440c4cd006916b558645b296ef0fd5aded3bba
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-11 01:20:49 -0700

    Instance Variables And Constants
    1. Used `const` for `gridSize` global data
    2. Discussed dependencies
    3. Refactored our code.

--- Tower Defense/Assets/Scripts/CubeEditor.cs
+++ Tower Defense/Assets/Scripts/CubeEditor.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-	[ExecuteInEditMode] // <-- execute in edit mode yeee
-	[SelectionBase] // makes selecting top quad of cube easier, this prevents me from clicking other quads.
+[ExecuteInEditMode] // <-- execute in edit mode yeee
+[SelectionBase] // makes selecting top quad of cube easier, this prevents me from clicking other quads.
+[RequireComponent(typeof(WayPoints))]
 public class CubeEditor : MonoBehaviour {
 
-	[SerializeField] [Range(1f, 20f)] float gridSize = 10f; // range allows a dragging function from 1 to 20.
+	const int gridSize = 10;
+	
+	/*This line of code is getting refactored *////[SerializeField] [Range(1f, 20f)] float gridSize = 10f; // range allows a dragging function from 1 to 20.
 	// you can also manually punch in the number into the float rather than use range also.
 
-	TextMesh textMesh;
+	// TextMesh textMesh;
+	//Vector3 gridPosition;
+	WayPoints waypoints; // waypoints = Waypoints  <--- script
 
 	void Start()
 	{
-
+	  waypoints = GetComponent<WayPoints>();
 	}
 
-	void Update () {
+	void Update () 
+	{
+		SnapToGrid();
+		UpdateLabel();
+	}
 	
-		Vector3 snapPosition;
+	private void SnapToGrid()
+		{
+		int gridSize = waypoints.GetGridSize(); // calling GetGridSize from waypoint script.
+		transform.position = new Vector3(waypoints.GetGridPosition().x, 0f , waypoints.GetGridPosition().y); 
+		}
 		
-		snapPosition.x = Mathf.RoundToInt (transform.position.x / gridSize) * gridSize; // transform.position.x is the x position of cube.
-		snapPosition.z = Mathf.RoundToInt (transform.position.z / gridSize) * gridSize;
-		transform.position = new Vector3(snapPosition.x, 0 , snapPosition.z); 
-		
-		textMesh = GetComponentInChildren<TextMesh>();
-		string textLabel =  snapPosition.x / gridSize + "," + snapPosition.z / gridSize; // text changes according to position of cube
+	private void UpdateLabel()
+		{
+	    TextMesh textMesh = GetComponentInChildren<TextMesh>(); // same as Textmesh textmesh on line 15.
+		int gridSize = waypoints.GetGridSize();
+		string textLabel =  waypoints.GetGridPosition().x / gridSize + "," + waypoints.GetGridPosition().y / gridSize; // text changes according to position of cube
 		textMesh.text = textLabel;
 		gameObject.name = textLabel;
-
-		
-
-		
-	}
+		}
 } // end of class
--- Tower Defense/Assets/Scripts/WayPoints.cs
+++ Tower Defense/Assets/Scripts/WayPoints.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class WayPoints : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	Vector2Int gridPos;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	const int gridSize = 10;
+
+	public int GetGridSize()
+	{
+		return gridSize;
 	}
-}
+
+	public Vector2 GetGridPosition()
+	{
+		return new Vector2Int(
+		  Mathf.RoundToInt (transform.position.x / gridSize) * gridSize, // transform.position.x is the x position of cube.
+		  Mathf.RoundToInt (transform.position.z / gridSize) * gridSize
+		);	
+	}
+} // end of class
