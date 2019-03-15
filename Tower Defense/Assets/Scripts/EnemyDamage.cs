d2a49345f194140ce954c43679dc0a44ab18c7d6
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-14 18:57:06 -0700

    Make Tower Shoot
    1. Attach a bullet particle effect to the towers.
    2. Create EnemyDamage script that recognises bullet collision.
    3. Organise enemy's hierarchical structure to properly recognise collision.

---
+++ Tower Defense/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour {
+
+	[SerializeField] Collider collisionMesh;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+/// OnParticleCollision is called when a particle hits a collider.
+/// </summary>
+/// <param name="other">The GameObject hit by the particle.</param>
+private	void OnParticleCollision(GameObject other)
+	{
+		print(" I'm hit yee");
+	}
+} // end of class.
