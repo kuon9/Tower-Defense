c895fa7ae3e367e227943bf4dfd8ea2d5662ed4d
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-15 06:01:08 -0700

    Enemy Health
    1. Decrease enemy hitpoints when enemy is hit.
    2. Destroy enemy when hitpoints are below 1.
    
    Updated prefab for Helicopter and Turrets

--- Tower Defense/Assets/Scripts/EnemyDamage.cs
+++ Tower Defense/Assets/Scripts/EnemyDamage.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour {
 
 	[SerializeField] Collider collisionMesh;
+	[SerializeField] int Health = 10; // can be float too.
 
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 /// OnParticleCollision is called when a particle hits a collider.
 /// </summary>
 /// <param name="other">The GameObject hit by the particle.</param>
-private	void OnParticleCollision(GameObject other)
-	{
-		print(" I'm hit yee");
-	}
+	
+	
+	private	void OnParticleCollision(GameObject other)
+		{
+			ProcessHit(); // this activates when the particle collide with the gameobject.
+			if(Health <= 0)
+			{
+				KillEnemy();
+			}
+		}
+	void ProcessHit()
+		{
+			Health = Health - 1;
+			print("current hitpoints are " + Health);
+	
+		}
+ 	private void KillEnemy()
+		{
+			Destroy(gameObject);
+		}
 } // end of class.
