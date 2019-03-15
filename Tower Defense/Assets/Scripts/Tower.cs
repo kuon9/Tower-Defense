5ff88b2a985a56d7efade91562f57d46807026b0
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-03-14 18:17:15 -0700

    Tower LookAt Enemy
    1. Construct a tower with base and top parts.
    2. Use LookAt to move the top of the tower to look at a moving enemy.

---
+++ Tower Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+
+[SerializeField] Transform Unit; // Which tower will do targeting pretty much. 
+[SerializeField] Transform targetEnemy; //you gonna put in the enemy in this slot so our towers look at them..\
+
+
+	// Update is called once per frame
+	void Update () {
+		Unit.LookAt(targetEnemy); // tower will look at enemy yee.
+	}
+} // end of class
