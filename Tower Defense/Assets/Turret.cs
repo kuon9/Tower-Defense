c31186f7fb5d1cb6fa5b8278d9ac5863770e3ff8
kuon9 <35052089+kuon9@users.noreply.github.com>
2019-04-09 18:38:47 -0700

    Turret Aim.
    Implemented new aimming script for Turret.

--- Tower Defense/Assets/Turret.cs
+++ Tower Defense/Assets/Turret.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour {
 
+	
+	public Transform Target;
+	public float turretRange = 50f;
+
+	public string enemyTag = "Enemy";
+	public Transform TurretPart;
+	public float turnSpeed = 10f;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		InvokeRepeating("UpdateTarget", 0f, 0.5f); // starting in 0 seconds and execute every 0.5 seconds
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	void UpdateTarget()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+		float shortestDistance = Mathf.Infinity; 
+		GameObject nearestEnemy = null;
 		
+		foreach(GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);	
+			if(distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+		if(nearestEnemy != null && shortestDistance <= turretRange)
+		{
+			Target = nearestEnemy.transform;
+		}
+
 	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Target == null)
+		return;
+
+		Vector3 dir = Target.position - transform.position; // dir = direction
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(TurretPart.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; // <--- eulerAngles = x, y,z.
+		TurretPart.rotation = Quaternion.Euler(0f,rotation.y,0f);
+	}	
+
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, turretRange); // draws lines on turret gameobject.
+	}
+
+
+
+	
 }
